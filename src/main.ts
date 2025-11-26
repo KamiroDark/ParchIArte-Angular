76c82f43fe19311957039e3034918c66ff991d9b
Kamiro <prietocamilo21@gmail.com>
2025-11-26 13:40:53 -0500

    Correcciones del main para que funcione

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app';
 
